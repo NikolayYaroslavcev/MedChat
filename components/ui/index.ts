0aa8b8e3bf6718e9e07550a46999fcf2aa515928
Nikolay Yaroslavcev <sweeterthanfurby@mail.ru>
2026-08-07 11:09:50 +0300

    feat(ui): add barrel export for components/ui

---
+++ components/ui/index.ts
@@ -0,0 +1,23 @@
+export { Button } from "./Button";
+export type { ButtonProps, ButtonVariant } from "./Button";
+
+export { Card } from "./Card";
+export type { CardProps, CardPadding } from "./Card";
+
+export { Badge } from "./Badge";
+export type { BadgeProps, BadgeVariant } from "./Badge";
+
+export { Input } from "./Input";
+export type { InputProps } from "./Input";
+
+export { StatusIndicator } from "./StatusIndicator";
+export type { StatusIndicatorProps, ConnectionState } from "./StatusIndicator";
+
+export { Container } from "./Container";
+export type { ContainerProps, ContainerSize } from "./Container";
+
+export { Divider } from "./Divider";
+export type { DividerProps } from "./Divider";
+
+export { LoadingDots } from "./LoadingDots";
+export type { LoadingDotsProps } from "./LoadingDots";
